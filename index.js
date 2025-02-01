a4d405a71c5e6a91581d34c9f602d2a8810becbf
Jb-git7 <sachintendulkarbritto@gmail.com>
2025-02-01 10:58:46 +0530

    Added Database

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import customerRoutes from "./routes/customers.route.js";
+import connectdB from "./lib/db.js";
 
 const app = express();
 app.use(express.json());
 
 const port = 5001;
+
+//connect DB
+
+connectdB();
 
 app.get("/", (req, res) => res.json({ msg: "Hello from express!!" }));
 
